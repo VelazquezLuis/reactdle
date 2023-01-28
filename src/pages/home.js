a2c8cb54f32b7efa51fb1ece9896975fe6a44480
Robert S <robscafe333@yahoo.com>
2023-01-27 17:11:19 -0800

    Setup react router with paths and also create pages
     added react router on app.js page and also created paths. Also created temporary pages to test out routing.

---
+++ src/pages/home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>You are now in the home page.</div>;
+};
+
+export default Home;
---
+++ src/pages/puzzleSolved.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PuzzleSolved = () => {
+  return <div> *** Congratulations *** The puzzle has been solved.</div>;
+};
+
+export default PuzzleSolved;
