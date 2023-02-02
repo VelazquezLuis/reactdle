6609dc395514adb63c60c6afb4eb74363c50149e
Felipe Fernandez <heyitsfelipe@gmail.com>
2023-02-02 11:50:27 -0800

    fix: resolve linting issues
    Fixes linting issues that occurred.

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,11 @@ import React from "react";
 import Board from "../components/Board/Board";
 
 const Home = () => {
-  return <div><Board /></div>;
+  return (
+    <div>
+      <Board />
+    </div>
+  );
 };
 
 export default Home;
