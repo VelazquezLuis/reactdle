2ee9579791603b96fe0bfefec3ffed9649181474
Felipe Fernandez <heyitsfelipe@gmail.com>
2023-02-12 21:19:08 -0800

    fix: add initial tiles
    Adds initial tiles to game board.

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,8 +1,17 @@
 import React from "react";
+import Tile from "../Tile/Tile";
 import "./board.css";
 
 const Board = () => {
-  return <div className="board">This is the board.</div>;
+  let tiles = [];
+  for (let i = 0; i < 5; i++) {
+    let row = [];
+    for (let j = 0; j < 5; j++) {
+      row.push(<Tile row={j} col={i} />);
+    }
+    tiles.push(<div className="board-row">{row}</div>);
+  }
+  return <div className="board">{tiles}</div>;
 };
 
 export default Board;
