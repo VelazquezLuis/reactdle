b77687281ed9adf17d024080d0683929e38a690f
luis velazquez <javiervelazquez113@yahoo.com>
2023-02-14 23:18:15 -0800

    ran pretier on appJS

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import Home from "./pages/home";
 import PuzzleSolved from "./pages/puzzleSolved";
 import Keyboard from "./components/Keyboard/keyboard";
 
-
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/puzzleSolved" element={<PuzzleSolved />} />
-
       </Routes>
-      <Keyboard/>
+      <Keyboard />
     </Router>
-    
   );
 }
 
