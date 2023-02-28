9dfb0d5aed7c40ec6b9dca31eba4ac177a1c9d02
luis velazquez <javiervelazquez113@yahoo.com>
2023-02-27 23:03:45 -0800

    Pull PR and pushing padding changes.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import PuzzleSolved from "./pages/puzzleSolved";
-import Keyboard from "./components/Keyboard/keyboard";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/puzzleSolved" element={<PuzzleSolved />} />
       </Routes>
-      <Keyboard />
     </Router>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Board from "../components/Board/Board";
+import Keyboard from "../components/Keyboard/keyboard";
+import Howtoplaymodal from "../components/Modals/howtoplaymodal";
 
 const Home = () => {
   return (
     <div>
       <Board />
+      <Keyboard />
+      <Howtoplaymodal />
     </div>
   );
 };
