02745d7834b501f8527a245557d152f9b85a91f8
luis velazquez <javiervelazquez113@yahoo.com>
2023-04-15 00:06:16 -0700

    Finished rules

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
 import PuzzleSolved from './pages/puzzleSolved'
-import Keyboard from "./components/Keyboard/keyboard";
 
 // import ReactSwitch from 'react-switch';
 export const ThemeContext = createContext(null);
