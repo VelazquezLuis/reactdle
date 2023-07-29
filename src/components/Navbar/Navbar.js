f08b2846847f0e5f6cd32939e8dd68a26a79501b
Robert S <robscafe333@yahoo.com>
2023-07-29 08:06:29 -0700

    Add Sound Icons
    removed sound switch and added sound icons in navbar. On and off speaker icons.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useCallback, useRef } from 'react';
 import './navbar.css';
 
 import { AiOutlineQuestionCircle } from 'react-icons/ai';
 import { BsFillGearFill } from 'react-icons/bs';
 import { GoThreeBars } from 'react-icons/go';
-
+import { HiOutlineSpeakerXMark } from 'react-icons/hi2';
+import { HiOutlineSpeakerWave } from 'react-icons/hi2';
+import music from '../../backgroundmusic.mp3';
 import { ThemeContext } from '../../App';
 
 const Navbar = () => {
   const { show, handleClose, handleShow } = useContext(ThemeContext);
+
+  const audioRef = useRef(null);
+  ////////////////
+
+  const [sound, setSound] = useState();
+
+  const handlePlaySoundOn = () => setSound(true);
+  const handlePlaySoundOff = () => setSound(false);
+
+  if (sound === true) {
+    console.log('sound is set to TRUE');
+    audioRef.current.play();
+  }
+  if (sound === false) {
+    console.log('sound is set to FALSE');
+    audioRef.current.pause();
+  }
+
   return (
     <div className="navbar-css">
       <div className="row">
@@ -17,9 +37,13 @@ const Navbar = () => {
         </div>
         <div className="col-lg-8">Wordle</div>
         <div className="col-lg-2 navrside">
+          <audio ref={audioRef} src={music}>
+            <track kind="captions" srcLang="en" label="English captions" />
+          </audio>
+          <HiOutlineSpeakerWave onClick={handlePlaySoundOn} />
+          <HiOutlineSpeakerXMark onClick={handlePlaySoundOff} />
           <AiOutlineQuestionCircle onClick={handleShow} />
           <span> </span>
-
           <BsFillGearFill onClick={handleShow} />
         </div>
       </div>
--- src/pages/home.js
+++ src/pages/home.js
@@ -10,20 +10,20 @@ import Howtoplaymodal from '../components/Modals/howtoplaymodal';
 
 const Home = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const audioRef = useRef(null);
+  // const [isPlaying, setIsPlaying] = useState(false);
+  // const audioRef = useRef(null);
 
-  const play = useCallback(() => {
-    setIsPlaying(true);
-    // access the audio element's play method using audioRef.current.play()
-    audioRef.current.play();
-  }, [audioRef]);
+  // const play = useCallback(() => {
+  //   setIsPlaying(true);
+  //   // access the audio element's play method using audioRef.current.play()
+  //   audioRef.current.play();
+  // }, [audioRef]);
 
-  const pause = useCallback(() => {
-    setIsPlaying(false);
-    // access the audio element's pause method using audioRef.current.pause()
-    audioRef.current.pause();
-  }, [audioRef]);
+  // const pause = useCallback(() => {
+  //   setIsPlaying(false);
+  //   // access the audio element's pause method using audioRef.current.pause()
+  //   audioRef.current.pause();
+  // }, [audioRef]);
 
   return (
     <div className="boardBackground">
@@ -33,11 +33,11 @@ const Home = () => {
         onChange={toggleTheme}
         checked={theme === 'dark'} // may serve another purpose. more research needed.
       />
-      <audio ref={audioRef} src={music}>
+      {/* <audio ref={audioRef} src={music}>
         <track kind="captions" srcLang="en" label="English captions" />
       </audio>
       Play Sound:
-      <ReactSwitch onChange={isPlaying ? pause : play} checked={isPlaying} />
+      <ReactSwitch onChange={isPlaying ? pause : play} checked={isPlaying} /> */}
       <div>
         <div className="board-wrapper">
           <Board />
