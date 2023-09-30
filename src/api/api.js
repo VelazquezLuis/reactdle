8792565f43db22fd1d08561f91bf0f4b0b5f5b79
Robert S <robscafe333@yahoo.com>
2023-09-29 22:03:27 -0700

    use react query
    replaced code so to use react query with axios.

--- src/api/api.js
+++ src/api/api.js
@@ -1,25 +1,29 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useQuery } from 'react-query';
 
 const useFetch = (url) => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { loading, error, data } = useQuery('repoData', () =>
+    fetch(url).then((res) => res.json())
+  );
+  // const [data, setData] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
 
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get(url)
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((err) => {
-        setError(err);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, [url]);
+  // useEffect(() => {
+  //   setLoading(true);
+  //   axios
+  //     .get(url)
+  //     .then((response) => {
+  //       setData(response.data);
+  //     })
+  //     .catch((err) => {
+  //       setError(err);
+  //     })
+  //     .finally(() => {
+  //       setLoading(false);
+  //     });
+  // }, [url]);
 
   return { data, loading, error };
 };
