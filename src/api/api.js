b3068ac1b3dadf195b71f8b1594d89574f03d0d3
Robert S <robscafe333@yahoo.com>
2023-10-05 08:59:00 -0700

    using axios instead of fetch, for practice

--- src/api/api.js
+++ src/api/api.js
@@ -3,9 +3,14 @@ import axios from 'axios';
 import { useQuery } from 'react-query';
 
 const useFetch = (url) => {
-  const { loading, error, data } = useQuery('repoData', () =>
-    fetch(url).then((res) => res.json())
-  );
+  const {
+    isLoading: loading,
+    error,
+    data,
+  } = useQuery('repoData', async () => {
+    const response = await axios.get(url);
+    return response.data;
+  });
 
   // if (loading) {
   //   console.log(loading);
